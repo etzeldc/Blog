7cc524d93da1bdf6b3fbf8a277fae8318e1a5221
etzeldc <etzeldc@gmail.com>
2019-07-31 19:25:21 -0400

    Code Clean Up
    Added a few small things and cleaned up some of the code.

--- Blog/Models/ViewModels/ViewModels.cs
+++ Blog/Models/ViewModels/ViewModels.cs
@@ -10,6 +10,7 @@ namespace Blog.Models.ViewModels
     public class EmailModel
     {
         [Required, Display(Name = "Name")]
+
         public string FromName { get; set; }
         [Required, Display(Name = "Email"), EmailAddress]
         public string FromEmail { get; set; }
